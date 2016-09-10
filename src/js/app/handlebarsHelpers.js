c691301b1d7a323288d4f3315c1c30a1cd95c96a
Edem Alloh <timreaperdnb@aol.com>
2016-09-10 13:12:01 +0100

    events page done and feedback from ant

--- src/js/app/handlebarsHelpers.js
+++ src/js/app/handlebarsHelpers.js
@@ -30,7 +30,59 @@ if (typeof module !== 'undefined') {
 		Handlebars.registerHelper('load_json', function (json_path) {
 			var fs = require('fs');
 			var obj = fs.readFileSync(json_path, 'utf-8');
-			return JSON.parse(obj.replace(/&quot;/g,'"'));
+			return JSON.parse(obj.replace(/&quot;/g, '"'));
 		});
-	};
+
+		/**
+		 * Each (Reverse)
+		 *      Does Handlebars each, but renders from bottom to top.
+		 ***/
+		Handlebars.registerHelper('each_reverse', function (context, options) {
+			var fn = options.fn, inverse = options.inverse;
+			var length = 0, ret = "", data;
+
+			if (Handlebars.Utils.isFunction(context)) {
+				context = context.call(this);
+			}
+
+			if (options.data) {
+				data = Handlebars.createFrame(options.data);
+			}
+
+			if (context && typeof context === 'object') {
+				if (Handlebars.Utils.isArray(context)) {
+					length = context.length;
+					for (var j = context.length - 1; j >= 0; j--) {//no i18n
+						if (data) {
+							data.index = j;
+							data.first = (j === 0);
+							data.last = (j === (context.length - 1));
+						}
+						ret = ret + fn(context[j], {data: data});
+					}
+				} else {
+					var keys = Object.keys(context);
+					length = keys.length;
+					for (j = length; j >= 0; j--) {
+						var key = keys[j - 1]
+						if (context.hasOwnProperty(key)) {
+							if (data) {
+								data.key = key;
+								data.value = context[key];
+								data.index = j;
+								data.first = (j === 0);
+							}
+							ret += fn(context[key], {data: data});
+						}
+					}
+				}
+			}
+
+			if (length === 0) {
+				ret = inverse(this);
+			}
+
+			return ret;
+		});
+	}
 }
